e89e437fa12f313c0eda2f586ad04a843dc51674
wjobayer <creativewebdeveloperjobayer@gmail.com>
2022-02-06 21:01:38 +0600

    add home page in App js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome To Hungry Kitchen</h1>
       <p>Contributor</p>
+      <Home></Home>
     </div>
   );
 }
