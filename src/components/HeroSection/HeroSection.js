328f8b014cc4a2fc622f0356824b2aa678ac398d
Alter80 <fahimatul8@Gmail.com>
2022-02-08 00:42:55 +0600

    check

---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroSection = () => {
+    return (
+        <div>
+            <h2>This is hero</h2>
+        </div>
+    );
+};
+
+export default HeroSection;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import HeroSection from "../../components/HeroSection/HeroSection";
 
 const Home = () => {
   return (
     <div>
       <h1>this is Home</h1>
+      <HeroSection></HeroSection>
     </div>
   );
 };
