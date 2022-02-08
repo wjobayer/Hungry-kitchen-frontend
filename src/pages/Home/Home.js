d34aba4b8b6dd67cb8d123456d67dde2c3e35561
habibshrabon <snhabib777@gmail.com>
2022-02-09 01:21:23 +0600

    mobile featured section added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import MobileFeatured from "../../components/MobileFeatured";
 import ProductCard from "../../components/ProductCard/ProductCard";
 const Home = () => {
   return (
     <div>
-      <h1>This is Home</h1>
       <ProductCard />
+      <MobileFeatured />
     </div>
   );
 };
