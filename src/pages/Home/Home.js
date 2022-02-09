7dc7327a7d709108b4f7b42104c25b295b718296
Alter80 <fahimatul8@Gmail.com>
2022-02-09 21:55:39 +0600

    develop updated

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
-import HeroSection from "../../components/HeroSection/HeroSection";
-=======
 import ParallaxSection from "../../components/ParallaxSection/ParallaxSection";
->>>>>>> 952f78e7b06c309031a1d8de6c86bcf842b3a86e
 import MobileFeatured from "../../components/MobileFeatured";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import HeroSection from "../../components/HeroSection/HeroSection";
 const Home = () => {
   return (
     <div>
