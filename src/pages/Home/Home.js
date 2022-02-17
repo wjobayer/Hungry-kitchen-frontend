8d9a6bde5e96db09dc49aa0c4aa42cfa9c6d5497
wjobayer <creativewebdeveloperjobayer@gmail.com>
2022-02-17 20:39:14 +0600

    dashboard

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,15 +5,21 @@ import ProductCard from "../../components/ProductCard/ProductCard";
 import HeroSection from "../../components/HeroSection/HeroSection";
 import PartnerSection from "../../components/PartnerSection/PartnerSection";
 import NewsLetter from "../../components/NewsLetter/NewsLetter";
+import ReactSlick from "../../components/ReactSlick/ReactSlick";
+import Header from "../../common/Header";
+import Footer from "../../common/Footer";
 const Home = () => {
   return (
     <div>
+      <Header></Header>
       <HeroSection></HeroSection>
       <ProductCard />
       <ParallaxSection />
       <PartnerSection />
       <NewsLetter />
       <MobileFeatured />
+      <ReactSlick></ReactSlick>
+      <Footer></Footer>
     </div>
   );
 };
