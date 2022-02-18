160b8b0824b3b7b878ea22b6115fb9b76b245ae3
Alter80 <fahimatul8@Gmail.com>
2022-02-19 04:04:34 +0600

    login page

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -4,6 +4,8 @@ import Footer from "./common/Footer";
 import Header from "./common/Header";
 import FoodDetails from "./components/FoodDetails/FoodDetails";
 import Home from "./pages/Home/Home";
+import Login from "./pages/LoginRegister/Login";
+import Register from "./pages/LoginRegister/Register";
 
 const AppRouter = () => {
   return (
@@ -13,6 +15,8 @@ const AppRouter = () => {
         <Route path="/" element={<Home />} />
         <Route path="details" element={<FoodDetails />} />
         <Route path="details/:id" element={<FoodDetails />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
       </Routes>
       <Footer />
     </Router>
