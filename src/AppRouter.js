b5be9f7caf46183a11974f530e36f99ffadfa69e
MdHasib01 <md.hasibuzzaman001@gmail.com>
2022-02-23 00:26:27 +0600

    search option emplement

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Footer from "./common/Footer";
+// import Header from "./common/Header";
 import FoodCategories from "./components/FoodCategories/FoodCategories";
 import FoodDetails from "./components/FoodDetails/FoodDetails";
 import Home from "./pages/Home/Home";
 import Login from "./pages/LoginRegister/Login";
 import Register from "./pages/LoginRegister/Register";
-// import Sidebar from "../src/components/Sidebar";
+import Sidebar from "../src/components/Sidebar";
 import Dashboard from "../src/pages/Dashboard";
 import Settings from "../src/pages/Settings";
 import Tables from "../src/pages/Tables";
 import Maps from "../src/pages/Maps";
-
 import DashboardMain from "./pages/DashboardMain";
+import SearchResult from "./components/SearchResult";
 
 const AppRouter = () => {
   return (
@@ -23,7 +25,9 @@ const AppRouter = () => {
         <Route path="details/:id" element={<FoodDetails />} />
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
+        <Route path="search" element={<SearchResult />} />
         {/* <Sidebar /> */}
+
         <Route path="/dashboard/*" element={<DashboardMain />} />
         <Route path="/" element={<Dashboard />} />
         <Route path="/settings" element={<Settings />} />
