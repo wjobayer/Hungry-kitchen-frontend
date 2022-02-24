306e888acd7e9cae871f54014befa4b80edbf480
toufiqulhoque <xtoufiqxx@gmail.com>
2022-02-24 20:35:30 +0600

    add to cart

---
+++ src/redux/slices/foodSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit'
+export const foodSlice = createSlice({
+  name: 'food',
+  initialState:{
+      addToCart:[],
+  },
+  reducers: {
+   addToCart:(state,{payload})=>{
+       state.addToCart.push(payload)
+   }
+  },
+})
+
+
+export const {addToCart} = foodSlice.actions
+
+export default foodSlice.reducer
